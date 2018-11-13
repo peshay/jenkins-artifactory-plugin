f65baecd62399b1b65d2153a1c06a44eed4b5116
Andreas Hubert <anhubert@gmail.com>
2018-11-13 13:55:52 +0100

    resolving variable build name in Maven3

--- src/main/java/org/jfrog/hudson/maven3/ArtifactoryMaven3Configurator.java
+++ src/main/java/org/jfrog/hudson/maven3/ArtifactoryMaven3Configurator.java
@@ -395,7 +395,13 @@ public class ArtifactoryMaven3Configurator extends BuildWrapper implements Deplo
             public boolean tearDown(AbstractBuild build, BuildListener listener) {
                 Result result = build.getResult();
                 if (deployBuildInfo && result != null && result.isBetterOrEqualTo(Result.SUCCESS)) {
-                    String buildName = BuildUniqueIdentifierHelper.getBuildNameConsiderOverride(ArtifactoryMaven3Configurator.this, build, null);
+                    EnvVars env;
+                    try {
+                        env = build.getEnvironment(listener);
+                    } catch (Exception e) {
+                        env = null;
+                    }
+                    String buildName = BuildUniqueIdentifierHelper.getBuildNameConsiderOverride(ArtifactoryMaven3Configurator.this, build, env);
                     build.getActions().add(new BuildInfoResultAction(getArtifactoryUrl(), build, buildName));
                     build.getActions().add(new UnifiedPromoteBuildAction(build, ArtifactoryMaven3Configurator.this));
                 }
